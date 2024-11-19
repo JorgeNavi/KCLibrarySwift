78a9c88a1dc0c1041596391e76af923787616d72
Jorge Navidad <jorge.espliego@outlook.com>
2024-11-19 19:47:38 +0100

    KeyChain changed

--- Sources/KCLibrarySwift/KeyChain.swift
+++ Sources/KCLibrarySwift/KeyChain.swift
@@ -1,8 +1,30 @@
-//
-//  File.swift
-//  KCLibrarySwift
-//
-//  Created by Jorge Navidad Espliego on 19/11/24.
-//
-
 import Foundation
+import Security
+import KeychainSwift
+
+public struct KeyChainKC {
+    
+    @discardableResult public func saveKC(_ key: String, value: String) -> Bool {
+        if let data = value.data(using: .utf8) {
+            let keychain = KeychainSwift()
+            return keychain.set(data, forKey: key)
+        } else {
+            return false
+        }
+        
+    }
+    
+    public func loadKC(_ key: String) -> String? {
+        let keychain = KeychainSwift()
+        if let data = keychain.get(key) {
+            return data
+        } else {
+            return ""
+        }
+    }
+    
+    @discardableResult public func deleteKC(_ key: String) -> Bool {
+        let keychain = KeychainSwift()
+        return keychain.delete(key)
+    }
+}
